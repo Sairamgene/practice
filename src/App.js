1a5cd11db9bb907d1f071c9bf4b167bf199f67f8
sairam <ram@abcgenesis.com>
2022-08-24 05:28:56 -0400

    added some in app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import Login from './component/Login';
-
+import Home from './component/Home';
 function App() {
   return (
     <div className="App">
    <h1>Heading towards the goal</h1>
    <h2>This is the my-new-v-branch</h2>
    <button>Trolled</button>
-   
+   <Home />
 <Login />
 
     </div>
