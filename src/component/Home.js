3e9a5bc2737c983049fe823a7cc5daa1564c78b8
sairam <ram@abcgenesis.com>
2022-08-24 05:22:23 -0400

    merged main with drills and added alt to image tag i home

--- src/component/Home.js
+++ src/component/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Home() {
   return (
     <div>
-      <img src='https://gitcheatsheet.org/images/logo.png'></img>
+      <img src='https://gitcheatsheet.org/images/logo.png' alt='git'></img>
       <h3>The new elements of github</h3>
       <h4>The command sheet for anything</h4>
     </div>
