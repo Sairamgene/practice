c11c4be4f81d9917ec0494aa4b0c96e1334fe1a7
sairam <ram@abcgenesis.com>
2022-08-24 04:20:47 -0400

    added some componets

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Login from './componenets/Login';
+
+function App() {
+  return (
+    <div className="App">
+   <h1>Heading towards the goal</h1>
+<Login />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componenets/Login.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div>
+      <div>
+      <label>user name</label>
+      <input type='text'></input>
+</div>
+
+      <div>
+      <label>Password</label>
+      <input type='password'></input>
+</div>
+<button type='submit'>Login</button>
+
+    </div>
+  )
+}
+
+export default Login
