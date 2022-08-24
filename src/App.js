bede0e18327de5707ce41bb109a7e153207bc718
sairam <ram@abcgenesis.com>
2022-08-24 04:33:18 -0400

    created  new branch my-new-branch and had some changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
-import Login from './componenets/Login';
+import Login from './component/Login';
 
 function App() {
   return (
     <div className="App">
    <h1>Heading towards the goal</h1>
+   <h2>This is the my-new-v-branch</h2>
+   <button>Trolled</button>
+   
 <Login />
 
     </div>
